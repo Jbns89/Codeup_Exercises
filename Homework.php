fd0acc96ec109f5391ff8fb80b95c66a59b0c511
Jbns89 <Jbns89@gmail.com>
2014-08-13 22:28:17 -0500

    exercise 4 arrays

--- Homework.php
+++ Homework.php
@@ -1,11 +1,18 @@
 <?php
-///////////Exercise 1
-
-///////////Exercise 2
-
 ///////////Exercise 3
-
+define(BIRTH_CITY,'New London ');
+$BIRTH_CITY='San Antonio';
+echo BIRTH_CITY . "\n";
 ///////////Exercise 4
+$prime_numbers=[2,3,5,7,11,13,17,19,23,29];
+echo $prime_numbers[3] . "\n";
+$car=['2004','Chevy','HHR','Silver','200,000'];
+$cars=
+	[
+		'Mike'=>'2004','Chevy','HHR','Silver','200,000',
+		'Angela'=>'2014','Ford','Mercury','Blue','201,000',
+		'Cy'=>'2004','Ford','Taurus','Red','202,000'
+	];
 
 ///////////Git
 
