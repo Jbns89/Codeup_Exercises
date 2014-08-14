5a48c93f4b94effd88ef3c2fcaa66afc43bfd9ab
Jbns89 <Jbns89@gmail.com>
2014-08-13 22:44:23 -0500

    exercise 4 var dump arrays fixed

--- Homework.php
+++ Homework.php
@@ -9,9 +9,9 @@ echo $prime_numbers[3] . "\n";
 $car=['2004','Chevy','HHR','Silver','200,000'];
 $cars=
 	[
-		'Mike'=>'2004','Chevy','HHR','Silver','200,000',
-		'Angela'=>'2014','Ford','Mercury','Blue','201,000',
-		'Cy'=>'2004','Ford','Taurus','Red','202,000'
+		'Mike' => ['year' => '2004','make' => 'Chevy', 'model' => 'HHR', 'color' => 'Silver', 'mileage' => '200,000'],
+		'Angela' => ['year' => '2014','make' => 'Ford', 'model' => 'Mercury', 'color' => 'Blue', 'mileage' => '201,000'],
+		'Cy' => ['year' => '2004','make' => 'Ford', 'model' => 'Taurus', 'color' => 'Red', 'mileage' => '202,000']
 	];
 var_dump($car);
 var_dump($cars);
