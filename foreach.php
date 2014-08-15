634a67919574ba5b80d53e918bdf2127cd8b2b21
Jbns89 <Jbns89@gmail.com>
2014-08-15 10:31:28 -0500

    printing out array

--- foreach.php
+++ foreach.php
@@ -32,12 +32,44 @@
 
 
 
+// $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+// foreach ($things as $data) 
+// {
+//     if (is_scalar($data)) 
+//     {
+//         echo "{$data} \n";
+//     }        
+    
+// }
+
+
+
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 foreach ($things as $data) 
 {
-    if (is_scalar($data)) 
+    if (is_int($data)) 
     {
         echo "{$data} \n";
-    }        
+    } 
+    else if (is_float($data)) 
+    {
+        echo "{$data} \n";
+    }
+    else if (is_bool($data)) 
+    {
+        echo "{$data}\n";
+    }
+    else if (is_array($data)) 
+    {
+        print_r($data);
+    }
+    else if (is_null($data)) 
+    {
+        echo "{$data} \n";
+    }
+    else if (is_string($data)) 
+    {
+        echo "{$data} \n";
+    }   
     
 }
