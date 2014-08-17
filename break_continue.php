e24edc5222797f808d11c99fd00f3320919bad16
Jbns89 <Jbns89@gmail.com>
2014-08-16 20:37:42 -0500

    Break and continue exercise

--- break_continue.php
+++ break_continue.php
@@ -1,11 +1,16 @@
 <?php
 for ($i = 1; $i <= 100; $i++) {
     
-    if ($i % 2 == 0) 
+    if ($i % 2 != 0) 
     {
-        echo $i;
-        echo "^ that is an even number.\n";
         continue;
     }
-    
+     echo "\n $i \n\n";
+}
+
+for ($i = 1; $i <= 100; $i++) {
+    echo $i . "\n";
+    if ($i == 10) {
+        break;
+    }
 }
