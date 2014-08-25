74b537d0f3475a80686dda3080cc7ca713cb3861
Jbns89 <Jbns89@gmail.com>
2014-08-24 19:43:15 -0500

    days of week

---
+++ switch.php
@@ -0,0 +1,56 @@
+<?php
+date_default_timezone_set('America/Chicago');
+$day_of_week=date('N', strtotime("-4 day"));
+switch($day_of_week)
+{
+	case 1:
+		echo 'Monday'.PHP_EOL;
+		break;
+	case 2:
+		echo 'Tuesday'.PHP_EOL;
+		break;
+	case 3:
+		echo 'Wednesday'.PHP_EOL;
+		break;
+	case 4:
+		echo 'Thursday'.PHP_EOL;
+		break;
+	case 5:
+		echo 'Friday'.PHP_EOL;
+		break;
+	case 6:
+		echo 'Saturday'.PHP_EOL;
+		break;
+	case 7:
+		echo 'Sunday'.PHP_EOL;
+		break;
+}
+
+if ($day_of_week==1) 
+	{
+		echo 'Monday'.PHP_EOL;
+	}
+elseif ($day_of_week==2) 
+	{
+		echo 'Tuesday'.PHP_EOL;
+	}
+elseif ($day_of_week==3) 
+	{
+		echo 'Wednesday'.PHP_EOL;
+	}
+elseif ($day_of_week==4) 
+	{
+		echo 'Thursday'.PHP_EOL;
+	}
+elseif ($day_of_week==5) 
+	{
+		echo 'Friday'.PHP_EOL;
+	}
+elseif ($day_of_week==6) 
+	{
+		echo 'Saturday'.PHP_EOL;
+	}
+elseif ($day_of_week==7) 
+	{
+		echo 'Sunday'.PHP_EOL;
+	}
