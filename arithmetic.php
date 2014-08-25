28d0805a1948e04a12465eada6b651bbda19ecc7
Jbns89 <Jbns89@gmail.com>
2014-08-24 19:37:46 -0500

    Math and functions

--- arithmetic.php
+++ arithmetic.php
@@ -1,18 +1,18 @@
 <?php
 function numeric_error($value1, $value2) 
 {
-		echo "Error! You must use numbers".PHP_EOL;			
+		return 'Error!'.PHP_EOL;			
 }
 function zero_error($value1) 
 {
-		echo "Error! You can't divide by zero".PHP_EOL;	
+		return 'Error!' .PHP_EOL;	
 }
 
 function add($a, $b)
 {
 	if (is_numeric($a)&& is_numeric($b)) 
 	{
-		echo $a + $b.PHP_EOL;
+		return $a + $b.PHP_EOL;
 	}	
 	else
 	{
@@ -23,7 +23,7 @@ function subt($a, $b)
 {
 	if (is_numeric($a)&& is_numeric($b)) 
 	{
-		echo $a - $b.PHP_EOL;
+		return $a - $b.PHP_EOL;
 	}	
 	else
 	{
@@ -34,7 +34,7 @@ function multi($a, $b)
 {
 	if (is_numeric($a)&& is_numeric($b)) 
 	{
-		echo $a * $b.PHP_EOL;
+		return $a * $b.PHP_EOL;
 	}	
 	else
 	{
@@ -48,11 +48,11 @@ function div($a, $b)
 		
 		if ($b==0) 
 		{
-			zero_error($b);
+			echo zero_error($b);
 		}
 		else
 		{
-			echo $a / $b.PHP_EOL;
+			return $a / $b.PHP_EOL;
 		}
 	}	
 	else
@@ -68,16 +68,16 @@ function remain($a, $b)
 		
 		if ($b==0) 
 		{
-			zero_error($b);
+			echo zero_error($b) .PHP_EOL;
 		}
 		else
 		{
-			echo $a % $b.PHP_EOL;
+			return $a % $b.PHP_EOL;
 		}
 	}	
 	else
 	{
-		echo numeric_error($a,$b), PHP_EOL;
+		echo numeric_error($a,$b) . PHP_EOL;
 	}
 
 }
