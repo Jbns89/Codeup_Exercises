d20352cdd5c823dbbcb82692baf11654fce20560
Jbns89 <Jbns89@gmail.com>
2014-08-24 19:39:11 -0500

    date functions

--- hwk_wk1_p2.php
+++ hwk_wk1_p2.php
@@ -1,25 +1,4 @@
 <?php
-// <!-- 2) hw_wk1_p2.php
-
-// Create an array that represents a quiz. In the top level array, 
-//the keys should be the question numbers and the values should be an 
-//associative array. The associative array should have the following 
-//keys: 'question', 'answers', 'correct_answer'. The 'question' is a 
-//string. The 'answers' is as associative array with alphabetical keys 
-//and string values. The 'correct_answer' is a string representing the 
-//letter of the correct answer. Make sure your quiz has at least 3 
-//questions.
-
-// Loop through the array and produces the following output:
-
-// 1. Question goes here.
-
-//    a. Answer one.
-//    b. Answer two.
-//   *c. Answer three. (Star before answer denotes that it is correct)
-//    d. Answer four.
-
-// ... -->
 
 $test=[
 
@@ -35,18 +14,19 @@ $test=[
 			'answers'=>['a'=> 'An African or European swallow?', 'b' => '112', 'c' => 42, 'd'=> 36],
 			'correct_answer'=>'a'
 		],
-	];
-
+	  ];
+$position=1;
 foreach ($test as $q_num => $questions) 
 {
-	echo $questions['question'];
+	if ($questions['answers']=$questions['correct_answer']) 
+	{
+		echo "*" . $questions['correct_answer'] . "\n";
+	}
+	else
+	{
+	echo "\n$position.   " . $questions['question']. "\n";
+	echo  "\ta." . $questions['answers']['a']. "\n\tb." . $questions['answers']['b'] . "\n\tc." . $questions['answers']['c'] . "\n\td." . $questions['answers']['d'] . "\n";
+	$position++;
+	}
 }
-// foreach($people as $person => $names)
-// {
-	
-// 	echo "\n $position. " . $names['name']['first'] . " " . $names['name']['last']; 
-// 	echo "\n    Phone: " . $names['phone_number'];
-// 	echo "\n    Address: " . $names['address'] . "\n";
-// 	$position++; 
 
-//}
